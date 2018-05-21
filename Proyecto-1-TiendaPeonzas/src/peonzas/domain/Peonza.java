bc85f8c35a9695dbbb11b21249062cba8b8583a1
FranciscoJBarrado <39370673+FranciscoJBarrado@users.noreply.github.com>
2018-05-21 11:47:42 +0200

    cambios peonza
    peonza

--- Proyecto-1-TiendaPeonzas/src/peonzas/domain/Peonza.java
+++ Proyecto-1-TiendaPeonzas/src/peonzas/domain/Peonza.java
@@ -2,12 +2,15 @@ package peonzas.domain;
 
 public class Peonza {
 
-	private String color;
+	private String nombre;
 	private int id;
 	private String material;
 	private int cantidad;
-	private String descripion;
+	private String descripcion;
+	private double precio;
 	private Cuerda cuerda;
+	private Punta punta;
+	private double tamanyo;
 	
 	
 	
@@ -16,35 +19,33 @@ public class Peonza {
 		
 	}
 	
-	public Peonza(String color, int peso, String marca, String material) {
-		this.color=color;
-		this.marca=marca;
+	public Peonza (String nombre, int id, String material, double tamanyo, double precio, int cantidad, String descripcion, Cuerda cuerda, Punta punta) {
+		this.cantidad=cantidad;
 		this.material=material;
-		this.peso=peso;
+		this.id=id;
+		this.descripcion=descripcion;
+		this.cuerda=cuerda;
+		this.punta=punta;
+		this.tamanyo=tamanyo;
+		this.nombre=nombre;
+		this.precio=precio;
+		
 	}
 
-	public String getColor() {
-		return color;
+	public String getNombre() {
+		return nombre;
 	}
 
-	public void setColor(String color) {
-		this.color = color;
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
 	}
 
-	public int getPeso() {
-		return peso;
+	public int getId() {
+		return id;
 	}
 
-	public void setPeso(int peso) {
-		this.peso = peso;
-	}
-
-	public String getMarca() {
-		return marca;
-	}
-
-	public void setMarca(String marca) {
-		this.marca = marca;
+	public void setId(int id) {
+		this.id = id;
 	}
 
 	public String getMaterial() {
@@ -55,10 +56,69 @@ public class Peonza {
 		this.material = material;
 	}
 
+	public int getCantidad() {
+		return cantidad;
+	}
+
+	public void setCantidad(int cantidad) {
+		this.cantidad = cantidad;
+	}
+
+	public String getDescripion() {
+		return descripcion;
+	}
+
+	public void setDescripion(String descripion) {
+		this.descripcion = descripion;
+	}
+
+	public double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+
+	public Cuerda getCuerda() {
+		return cuerda;
+	}
+
+	public void setCuerda(Cuerda cuerda) {
+		this.cuerda = cuerda;
+	}
+
+	public Punta getPunta() {
+		return punta;
+	}
+
+	public void setPunta(Punta punta) {
+		this.punta = punta;
+	}
+
+	public double getTamanyo() {
+		return tamanyo;
+	}
+
+	public void setTamanyo(double tamanyo) {
+		this.tamanyo = tamanyo;
+	}
+
 	@Override
 	public String toString() {
-		return "Peonza [color=" + color + ", peso=" + peso + ", marca=" + marca + ", material=" + material + "]";
+		return "Datos de la peonza: " 
+				+ "\n\t1.Id: " + id
+				+ "\n\t2.Nombre: " + nombre 
+				+ "\n\t3.Descripción: " + descripcion
+				+ "\n\t4.Material " + material 
+				+ "\n\t5.Tamaño: " + tamanyo + " cm"
+				+ "\n\t5.Cantidad: " + cantidad + " unidades"
+				+ "\n\t6.Precio: " + precio + " € "
+				+ "\n\t7.Cuerda: " + cuerda 
+				+ "\n\t8.Punta: " + punta;
 	}
+
+	
 	
 	
 }

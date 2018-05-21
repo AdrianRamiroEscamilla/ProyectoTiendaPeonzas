a39b491bf13cc3e466d0acfee96dfae7f5d01071
FranciscoJBarrado <39370673+FranciscoJBarrado@users.noreply.github.com>
2018-05-21 11:27:47 +0200

    cuerda
    cuerda

---
+++ Proyecto-1-TiendaPeonzas/src/peonzas/domain/Cuerda.java
@@ -0,0 +1,64 @@
+package peonzas.domain;
+
+public class Cuerda {
+	
+	private String nombre;
+	private double longitud;
+	private double grosor;
+	private int id;
+	
+	public Cuerda(){
+		
+	}
+	
+	public Cuerda(String nombre, double longitud, double grosor, int id){
+		this.id=id;
+		this.longitud=longitud;
+		this.nombre=nombre;
+		this.grosor=grosor;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public double getLongitud() {
+		return longitud;
+	}
+
+	public void setLongitud(double longitud) {
+		this.longitud = longitud;
+	}
+
+	public double getgrosor() {
+		return grosor;
+	}
+
+	public void setgrosor(double grosor) {
+		this.grosor = grosor;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	@Override
+	public String toString() {
+		return "Características de la cuerda: "
+				+ "\n\t1.Id: " + id + ""
+				+ "\n\t2.Nombre: " + nombre + ""
+				+ "\n\t2.Longitud:" + longitud + " cm."
+				+ "\n\t3.Grosor: " + grosor + " cm.";
+				
+	}
+	
+	
+}
---
+++ Proyecto-1-TiendaPeonzas/src/peonzas/domain/Peonza.java
@@ -0,0 +1,64 @@
+package peonzas.domain;
+
+public class Peonza {
+
+	private String color;
+	private int id;
+	private String material;
+	private int cantidad;
+	private String descripion;
+	private Cuerda cuerda;
+	
+	
+	
+	
+	public Peonza(){
+		
+	}
+	
+	public Peonza(String color, int peso, String marca, String material) {
+		this.color=color;
+		this.marca=marca;
+		this.material=material;
+		this.peso=peso;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public int getPeso() {
+		return peso;
+	}
+
+	public void setPeso(int peso) {
+		this.peso = peso;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public String getMaterial() {
+		return material;
+	}
+
+	public void setMaterial(String material) {
+		this.material = material;
+	}
+
+	@Override
+	public String toString() {
+		return "Peonza [color=" + color + ", peso=" + peso + ", marca=" + marca + ", material=" + material + "]";
+	}
+	
+	
+}

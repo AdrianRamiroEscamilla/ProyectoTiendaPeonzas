e8678452cfc6bc9aba9e65d580e23ffb728fa02e
FranciscoJBarrado <39370673+FranciscoJBarrado@users.noreply.github.com>
2018-05-21 11:52:48 +0200

    peonza control
    control

--- Proyecto-1-TiendaPeonzas/src/peonzas/control/PeonzaSelect.java
+++ Proyecto-1-TiendaPeonzas/src/peonzas/control/PeonzaSelect.java
@@ -1,48 +1,27 @@
 package peonzas.control;
 
 import java.io.IOException;
-
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import peonzas.domain.Peonza;
-import peonzas.modelo.PeonzaExpert;
-
-import java.util.*;
-
-
 /**
  * Servlet implementation class PeonzaSelect
  */
-@WebServlet(
-        name = "PeonzaSelect",
-        urlPatterns = {"/PeonzaSelect.do"},
-        asyncSupported = false)
+@WebServlet("/PeonzaSelect")
 public class PeonzaSelect extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-	protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-		/**
-	     * @see HttpServlet#HttpServlet()
-	     */
-		//PASO 01: Recoger informacion
-	    String c = request.getParameter("material");
-	    PeonzaExpert be = new PeonzaExpert();
-	    
-	    //PASO 02: Recopilar la respuesta
-	    List<Peonza> result = be.getBrands(c);
-	    request.setAttribute("styles", result);
-	    
-	    //PASO 03: Salir      
-	    RequestDispatcher view = request.getRequestDispatcher("Result.jsp");
-	    view.forward(request, response);
-	    //request.getRequestDispatcher("result.jsp").forward(request, response);
-	}
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public PeonzaSelect() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */

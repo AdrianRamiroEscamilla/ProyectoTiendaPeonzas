eec794c861c9d39afb6def33ba5f87aa3445774f
FranciscoJBarrado <39370673+FranciscoJBarrado@users.noreply.github.com>
2018-05-21 16:33:01 +0200

    cambios metodos
    sdfsd

--- Proyecto-1-TiendaPeonzas/src/peonzas/servicios/PeonzasMetodos.java
+++ Proyecto-1-TiendaPeonzas/src/peonzas/servicios/PeonzasMetodos.java
@@ -1,5 +1,6 @@
 package peonzas.servicios;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import peonzas.domain.Cuerda;
@@ -9,27 +10,31 @@ import peonzas.domain.Punta;
 public class PeonzasMetodos {
 
 	
-
+	List <Peonza> ListaPeonzas= new ArrayList<>();
+	List <Cuerda> ListaCuerdas= new ArrayList<>();
+	List <Punta> ListaPuntas= new ArrayList<>();
+	
 	/**
 	 * metodo para mostrar la lista de peonzas
 	 * @return
 	 */
-	public void muestraPeonzas(ListaPeonzas){
+	public void muestraPeonzas(){
 		
 		for(Peonza peonza:ListaPeonzas){
 			System.out.println(peonza.toString());
 		}
-		
-		
-		
 	}
 	
 	/**
 	 * metodo para buscar peonzas
 	 * @return
 	 */
-	public List<Peonza> buscaPeonzas(){
-		
+	public List<Peonza> buscaPeonzas(String material){
+		for(int i=0;i<ListaCuerdas.size();i++){
+			if(ListaPeonzas.get(i).equals(material)){
+				System.out.println(ListaCuerdas.get(i));
+			}
+		}
 		return ListaPeonzas;
 		
 	}
@@ -39,6 +44,7 @@ public class PeonzasMetodos {
 	 * @return
 	 */
 	public void muestraPuntas(){
+		
 		
 		for(Punta punta:ListaPuntas){
 			System.out.println(punta.toString());

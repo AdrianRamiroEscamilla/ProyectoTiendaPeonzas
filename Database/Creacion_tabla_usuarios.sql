307c9edb6776297d6b69b0103ff0a4b059432b86
DDonamaria <39370586+DDonamaria@users.noreply.github.com>
2018-05-23 14:23:49 +0200

    d

--- Database/Creacion_tabla_usuarios.sql
+++ Database/Creacion_tabla_usuarios.sql
@@ -1,13 +1,16 @@
 USE proyectopeonzas;
 
+DROP TABLE IF EXISTS `usuarios`;
+
 -- table usuarios
 CREATE TABLE  `usuarios` (
   `idUsuario` int(3) NOT NULL AUTO_INCREMENT,
-  `nombre_usuario` varchar(10) NOT NULL,
-  `email` varchar(10) NOT NULL,
+  `nombre_usuario` varchar(10) NOT NULL UNIQUE,
+  `email` varchar(50) NOT NULL UNIQUE,
   `contraseña` varchar(15) BINARY NOT NULL,
   `nombre` varchar(10),
-  `Apellidos` varchar(50),
+  `apellidos` varchar(50),
+  `administrador` boolean NOT NULL,
     
     PRIMARY KEY (`idUsuario`)
 
---
+++ Database/Insert Usuarios.sql
@@ -0,0 +1,5 @@
+INSERT INTO `proyectopeonzas`.`usuarios` (`nombre_usuario`,`email`,`contraseña`,`nombre`,`apellidos`,`administrador`) VALUES ('Admin1','admin1@correo.com','1111','Andrea','Cuellar Mateo','1');
+INSERT INTO `proyectopeonzas`.`usuarios` (`nombre_usuario`,`email`,`contraseña`,`nombre`,`apellidos`,`administrador`) VALUES ('Usuario1','usuario1@correo.com','1111','Luis','Mendoza Ruiz','0');
+INSERT INTO `proyectopeonzas`.`usuarios` (`nombre_usuario`,`email`,`contraseña`,`nombre`,`apellidos`,`administrador`) VALUES ('Usuario2','usuario2@correo.com','1111','Javier','Del Olmo Garcia','0');
+
+SELECT * FROM usuarios;

2c731b2f591a22f76e213c85c3084b00269e9ef5
Adrian Ramiro <33880273+AdrianRamiroEscamilla@users.noreply.github.com>
2018-05-25 11:34:46 +0200

    t

--- Proyecto-1-TiendaPeonzas/src/peonzas/datos/DatosPeonzas.java
+++ Proyecto-1-TiendaPeonzas/src/peonzas/datos/DatosPeonzas.java
@@ -16,7 +16,6 @@ import utilidades.Acceso;
 
 public class DatosPeonzas {
 	ArrayList<Peonza> alPeonzas = new ArrayList<>();
-	
 
 	public ArrayList<Peonza> seePeonza() {
 
@@ -27,11 +26,11 @@ public class DatosPeonzas {
 
 		ResultSet rs = Acceso.consultMySql(ruta, usuario, password, query);
 		Peonza peonza;
-		Punta punta ;
-		Cuerda cuerda ;
+		Punta punta;
+		Cuerda cuerda;
 		try {
 			while (rs.next()) {
-				peonza= new Peonza();
+				peonza = new Peonza();
 				punta = new Punta();
 				cuerda = new Cuerda();
 				peonza.setId(rs.getInt(1));
@@ -58,31 +57,29 @@ public class DatosPeonzas {
 		return alPeonzas;
 
 	}
+
 	/**
-	 * buscaCategoria
-	 * Sirve para buscar en todas las categorías del menú vertical derecho.
+	 * buscaCategoria Sirve para buscar en todas las categorías del menú
+	 * vertical derecho.
 	 * 
 	 * @param categoria
 	 * @param tipo
 	 * @return ArrayList<Peonza>
 	 */
-	
-	public ArrayList<Peonza> buscaCategoria(String categoria, String tipo ) {
+
+	public ArrayList<Peonza> buscaCategoria(String categoria, String tipo) {
 		ArrayList<Peonza> apeonzas = new ArrayList<>();
 
-		if (categoria.equals("material")){
-			tipo = "='"+tipo+"'";
+		if (categoria.equals("material")) {
+			tipo = "='" + tipo + "'";
+		} else if (categoria.equals("punta")) {
+			tipo = "='" + tipo + "'";
 		}
-		else if(categoria.equals("punta"))
-		{
-			tipo = "='"+tipo+"'";
+		if (categoria.equals("cuerda")) {
+			tipo = "='" + tipo + "'";
 		}
-		if(categoria.equals("cuerda"))
-		{
-			tipo = "='"+tipo+"'";
-		}
-	
-		String query = "Select * from peonzas WHERE "+categoria+tipo;
+
+		String query = "Select * from peonzas WHERE " + categoria + tipo;
 		String ruta = "jdbc:mysql://10.90.36.16/proyectopeonzas";
 		String usuario = "admin";
 		String password = "1111";
@@ -93,7 +90,7 @@ public class DatosPeonzas {
 		Punta punta = new Punta();
 		try {
 			while (rs.next()) {
-				peonza= new Peonza();
+				peonza = new Peonza();
 				peonza.setId(rs.getInt(1));
 				peonza.setNombre(rs.getString(2));
 				peonza.setTamanyo(rs.getDouble(3));
@@ -116,24 +113,22 @@ public class DatosPeonzas {
 		System.out.println(apeonzas.size());
 
 		return apeonzas;
-	
-	}
-	
 
-	public Peonza searchId(String idPeonza){
-		
-		String query = "Select * from peonzas WHERE idPeonza ='"+idPeonza+"'";
+	}
+
+	public Peonza searchId(String idPeonza) {
+
+		String query = "Select * from peonzas WHERE idPeonza ='" + idPeonza + "'";
 		String ruta = "jdbc:mysql://10.90.36.16/proyectopeonzas";
 		String usuario = "admin";
 		String password = "1111";
 
 		ResultSet rs = Acceso.consultMySql(ruta, usuario, password, query);
+		System.out.println(rs);
 		Peonza peonza = new Peonza();
 		Punta punta = new Punta();
 		Cuerda cuerda = new Cuerda();
 		try {
-			
-			while(rs.next()){
 			peonza.setId(rs.getInt(1));
 			peonza.setNombre(rs.getString(2));
 			peonza.setTamanyo(rs.getInt(3));
@@ -147,9 +142,11 @@ public class DatosPeonzas {
 			peonza.setCuerda(cuerda);
 			peonza.setDescripcion(rs.getString(10));
 			System.out.println(peonza.toString());
+			while (rs.next()) 
+			{
+				
 			}
-		}catch(SQLException e)
-		{
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 		return peonza;
@@ -157,68 +154,64 @@ public class DatosPeonzas {
 
 	/**
 	 * M�todo para el backoffice para a�adir productos
+	 * 
 	 * @param peonza
 	 * @param punta
 	 * @param cuerda
 	 * @return
 	 */
-	public boolean altaPeonza(Peonza peonza, int punta, int cuerda){
+	public boolean altaPeonza(Peonza peonza, int punta, int cuerda) {
 		ArrayList<Peonza> apeonzas = new ArrayList<>();
-		
-		boolean realizado = false;
-		String query2 = "INSERT INTO peonzas ( nombrePeonza, tamanyo, precio, imagen, material, cantidad, punta, cuerda, descripcion) values('"+peonza.getNombre()+"', '"+peonza.getTamanyo()+"', '"+peonza.getPrecio()+"', '"+peonza.getMaterial()+"','"+peonza.getImagen()+"', '"+peonza.getCantidad()+"', '"+punta+"', '"+cuerda+"', '"+peonza.getDescripcion()+"')";
 
-		String ruta = "jdbc:mysql://10.90.36.16/proyectopeonzas";     
+		boolean realizado = false;
+		String query2 = "INSERT INTO peonzas ( nombrePeonza, tamanyo, precio, imagen, material, cantidad, punta, cuerda, descripcion) values('"
+				+ peonza.getNombre() + "', '" + peonza.getTamanyo() + "', '" + peonza.getPrecio() + "', '"
+				+ peonza.getMaterial() + "','" + peonza.getImagen() + "', '" + peonza.getCantidad() + "', '" + punta
+				+ "', '" + cuerda + "', '" + peonza.getDescripcion() + "')";
+
+		String ruta = "jdbc:mysql://10.90.36.16/proyectopeonzas";
 		String usuario = "admin";
 		String password = "1111";
-		
+
 		try {
 			int modif = Acceso.modifMySql(ruta, usuario, password, query2);
-			if(modif == 1)
-			{
+			if (modif == 1) {
 				realizado = true;
-			}else if(modif == -3)
-			{
+			} else if (modif == -3) {
 				realizado = false;
 			}
 
-			
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		System.out.println(apeonzas.size());
 		return realizado;
-		
-
-	
-		
 
 	}
-	
+
 	/**
 	 * M�todo para el backoffice para eliminar productos
+	 * 
 	 * @param peonza
 	 * @param punta
 	 * @param cuerda
 	 * @return
 	 */
-	public boolean deletePeonza(Peonza peonza){
-		
-		boolean realizado = false;
-		String query2 = "delete from peonzas where idPeonza='"+peonza.getId()+"'";
+	public boolean deletePeonza(Peonza peonza) {
 
-		String ruta = "jdbc:mysql://10.90.36.16/proyectopeonzas";     
+		boolean realizado = false;
+		String query2 = "delete from peonzas where idPeonza='" + peonza.getId() + "'";
+
+		String ruta = "jdbc:mysql://10.90.36.16/proyectopeonzas";
 		String usuario = "admin";
 		String password = "1111";
-		
+
 		try {
 			int modif = Acceso.modifMySql(ruta, usuario, password, query2);
-			if(modif == 1)
-			{
+			if (modif == 1) {
 				realizado = true;
-			}else if(modif == -3)
-			{
+			} else if (modif == -3) {
 				realizado = false;
 			}
 		} catch (SQLException e) {
@@ -227,7 +220,6 @@ public class DatosPeonzas {
 		}
 		return realizado;
 
-
 	}
-	
+
 }
